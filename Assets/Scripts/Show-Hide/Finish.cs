d629e46a09510ff9cf35469fff54f7634bbd62eb
laurenmaher13@hotmail.com <laurenmaher13@hotmail.com>
2020-05-25 15:26:45 +0100

    25-5-2020

--- Assets/Scripts/Show-Hide/Finish.cs
+++ Assets/Scripts/Show-Hide/Finish.cs
@@ -4,13 +4,15 @@ public class Finish : MonoBehaviour
 {
    
     public GameObject Finished;
+    public GameObject Finished2;
 
 
     void OnMouseDown()
     {
       
             Finished.SetActive(true);
-   
+
+        Finished2.SetActive(true);
 
     }
 }
